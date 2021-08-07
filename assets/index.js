ed0488605eccacc16743ab9d83992a8a1947c23a
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2021-08-07 19:03:22 +0700

    Update index.js
    changed some things
    shorten some things to save space because why not

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,30 @@
-var navbar_unlock,prevScrlPos = window.pageYOffset;
-function retheme(){if(document.body.className.indexOf("bright") > -1){document.body.className=document.body.className.replace("bright","")}else{document.body.className+=" bright"}}
-window.onscroll = function(){
-var currScrlPos = window.pageYOffset;
-  if (prevScrlPos > currScrlPos){navbar.style.top = 0;navbar_unlock = false}
-  else {navbar.style.top = "-50px";navbar_unlock = true}
+let navbar_unlock,prevScrlPos = window.pageYOffset;
+window.matchMedia("(prefers-color-scheme: dark)").addListener(e=>{
+	if (e.matches){
+		document.body.className=document.body.className.replace("bright","");
+		rethemeBtn.innerText = "🌘️";
+	} else {
+		document.body.className += " bright";
+		rethemeBtn.innerText = "☀️";
+	}
+});
+retheme=()=>{
+	document.body.classList.toggle("bright");
+	if (document.body.className.indexOf("bright") > -1){
+		rethemeBtn.innerText = "🌘️";
+	} else {
+		rethemeBtn.innerText = "☀️";
+	}
+}
+window.onscroll=()=>{
+let currScrlPos = window.pageYOffset;
+  if (prevScrlPos > currScrlPos){navbar.style.top = 0;navbar_unlock = 0}
+  else {navbar.style.top = "-50px";navbar_unlock = 1}
   prevScrlPos = currScrlPos;
 }
 /* Bug Fixed: Scroll Down, Then Little bit up, Then Move Mouse. */
-window.onmousemove = function(){
-	if (window.event.clientY < 46){navbar.style = "top:0"}
-	else if (window.event.clientY > 46 && navbar_unlock){navbar.style.top = "-50px"}
+window.onmousemove=()=>{
+	if(window.event.clientY < 46){navbar.style = "top:0"}
+	else if(window.event.clientY > 46 && navbar_unlock){navbar.style.top = "-50px"}
 }
-
-searchInput.onkeyup = function(e){
-  if (e.keyCode == 13){
-   e.preventDefault();
-   submitSearch.click();
-   searchInput.value = ''
-  }
-};
+searchInput.onkeyup=e=>{if(e.which == 13){e.preventDefault();submitSearch.click();searchInput.value = ''}};
