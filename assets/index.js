2f8a86bb7cb638aa76579cf13fe42af666350f5f
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2024-12-12 16:59:44 +0700

    Update the website
    Lots of changes, see index.html, line 34 for detail

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,4 @@
+"use strict";
 window.addEventListener("load",_ => {
 	let navbar_unlock,
 			prevScrlPos = window.pageYOffset,
@@ -9,7 +10,7 @@ window.addEventListener("load",_ => {
 		document.body.className += " bright";
 		rethemeBtn.innerText = "☀️";
 	};*/
-	retheme=()=>{
+	window.retheme=()=>{
 		document.body.classList.toggle("light");
 		if (document.body.className.indexOf("light") > -1){
 			rethemeBtn.innerText = "🌘️";
@@ -30,45 +31,57 @@ window.addEventListener("load",_ => {
 	//searchInput.onkeyup=e=>{if(e.which == 13){e.preventDefault();submitSearch.click();searchInput.value = ''}};
 
 	// Simple fast memoize
-	window.memoize=f=>{let k,c={__proto__:null};return(...a)=>{k=JSON.stringify(a);return(c[k]=c[k]||f(...a))}};
+	window.memoize=(f,h)=>{const c={__proto__:null};h=h??JSON.stringify;return(...a)=>{const k=h(a);return c[k]||(c[k]=f(...a))}}
 
-	// Scramble text (optionally depends on memoize to reduce lag)
-	window.scrambleText=(()=>{let e,t,s="";return this.rndTxt=memoize((function(){for(s="",e=0;e<this.queue.length;e++){let{from:i,to:h,start:r,end:u,char:a}=this.queue[e];this.frame>=u?(t++,s+=h):this.frame>=r?((!a||Math.random()<.3)&&(this.queue[e].char=a="!<>\\/[]{}-_—=+*^?#"[~~(18*Math.random())]),s+=`<span style="opacity: 0.4">${a}</span>`):s+=i}return s})),this.modQueue=memoize(((t,s)=>{let i;for(this.queue=[],e=0,length=Math.max(t.length,s.length);e<length;e++)i=~~(10*Math.random()),this.queue.push({from:t[e]||"",to:s[e]||"",start:i,end:i+~~(10*Math.random())})})),this.update=e=>{t=0,this.el.innerHTML=this.rndTxt(e+this.frame),t===this.queue.length?this.resolve():(this.frameRequest=setTimeout((()=>requestAnimationFrame(this.update)),50),this.frame++)},this.clearQueue=()=>{this.queue=[],clearTimeout(this.frameRequest)},this.setText=(e,t)=>{this.el=t,this.frame=0,this.promise=new Promise((e=>this.resolve=e));let s=t.innerText;return this.modQueue(s,e),clearTimeout(this.frameRequest),this.update(s+e),this.promise},this.setText.clearQueue=this.clearQueue,this.setText})();
+	// Text scrambler effect
+	class TextScramble{constructor(e){this.el=e,this.chars="!<>\\/[]{}-_—=+*^?#",this.update=this.update.bind(this),this.isCompleteNewText=!1,memoize&&(this.oldNewTextCacheKey="",this.randomText=memoize(this.randomText.bind(this),(e=>e)))}setText(e){const t=Math.random,s=this.el.innerText,h=Math.max(s.length,e.length),i=new Promise((e=>this.resolve=e));this.oldNewTextCacheKey=s+","+e,this.queue=[];for(let i=0;i<h;i++){const h=~~(10*t());this.queue.push({from:s[i]||"",to:e[i]||"",start:h,end:h+~~(10*t())})}return clearTimeout(this.frameRequest),this.frame=0,this.isCompleteNewText=!1,this.update(),i}update(){this.el.innerHTML=this.randomText(this.oldNewTextCacheKey+this.frame),this.isCompleteNewText?(this.isCompleteNewText=!1,this.resolve()):(this.frameRequest=setTimeout((()=>requestAnimationFrame(this.update)),50),this.frame++)}randomText(){let e="",t=0;const s=this.queue.length,h=Math.random;for(let i=0;i<s;i++){let{from:s,to:a,start:r,end:o,char:m}=this.queue[i];this.frame>=o?(t++,e+=a):this.frame>=r?((!m||h()<.3)&&(this.queue[i].char=m=this.chars[~~(Math.random()*this.chars.length)]),e+=`<span style="opacity: 0.4">${m}</span>`):e+=s}return t===s&&(this.isCompleteNewText=!0),e}}
 
-	// Header animation
-	document.querySelector("section#home header.header > .header__title").innerText = "";
-	document.querySelector("section#home header.header > .header__desc").innerText = "";
-	scrambleText("MDP Website",document.querySelector("section#home header.header > .header__title")).then(()=>{
-		scrambleText("Welcome To My Website.",document.querySelector("section#home header.header > .header__desc"))
+	// Header text scramble
+	const header__title = document.body.querySelector("header > .header__title");
+	const header__desc  = document.body.querySelector("header > .header__desc");
+	const ts__headerTitle = new TextScramble(header__title);
+	const ts__headerDesc  = new TextScramble(header__desc);
+	header__title.innerText = "";
+	header__desc.innerText = "";
+	document.head.querySelector("title").textContent = "MDP website"
+	ts__headerTitle.setText("MDP website").then(() => {
+		setTimeout(()=>{
+			ts__headerTitle.setText("MDP43140's website");
+			document.head.querySelector("title").textContent = "MDP43140's website"
+		},500)
+		ts__headerDesc.setText("Hey there!").then(() => {
+			setTimeout(()=>{
+				ts__headerDesc.setText("Welcome to my website!",header__desc)
+			},500)
+		});
 	});
 
 	// Ripple Effect
 	(()=>{"use strict";for(let e of document.querySelectorAll("button, nav#bar > a")){let t,o,n=0,i=e,r=e=>{if(n>1||t)return;n++,t=setTimeout((()=>t=null),50),o=i.getBoundingClientRect();let r={el:document.createElement("div")},l=e.pageX?e.pageX-o.left:o.width/2,a=e.pageY?e.pageY-o.top:o.height/2;r.el.style=`pointer-events:none;top:${a}px;left:${l}px;position:absolute;opacity:.4;background:#888;border-radius:50%;width:5px;height:5px`,r.tmp0=`scale(${Math.max(i.offsetWidth,i.offsetHeight)/2})`,r.el.animate([{transform:r.tmp0}],{duration:400,easing:"ease-out"}).onfinish=e=>{r.el.style.transform=r.tmp0,r.dA=1},i.append(r.el),r.removeEl=e=>{clearTimeout(r.removeElTimeout),r.dA?(window.removeEventListener("pointerup",r.removeEl),i.removeEventListener("keyup",r.removeEl),r.el.animate([{opacity:0}],{duration:200,easing:"ease-out"}).onfinish=e=>{r.el.remove(),r=null,n--}):r.removeElTimeout=setTimeout((e=>{r.removeEl()}))},window.addEventListener("pointerup",r.removeEl),i.addEventListener("keyup",r.removeEl)};e.style.position="relative",e.style.overflow="hidden",e.addEventListener("pointerdown",r),e.addEventListener("keydown",r)}})();
 
-	// Edge lighting Cool effect by Hyperplexed
-	document.querySelector("body > section > nav#bar").onmousemove = e => {
-		for(const card of document.querySelectorAll("body > section > nav#bar > a")){
-			const rect = card.getBoundingClientRect(),
-						x = e.clientX - rect.left,
-						y = e.clientY - rect.top;
-			card.style.setProperty("--cursor-x",`${x}px`);
-			card.style.setProperty("--cursor-y",`${y}px`);
+	// (oversimplified) Edge lighting effect, by Hyperplexed
+	const edgeLightingEls = document.querySelectorAll("body > nav#bar > a")
+	document.querySelector("body > nav#bar").onmousemove = e => {
+		for (const el of edgeLightingEls){
+			const rect = el.getBoundingClientRect();
+			el.style.setProperty("--cursor-x",`${e.clientX - rect.left}px`);
+			el.style.setProperty("--cursor-y",`${e.clientY - rect.top }px`);
 		};
 	}
 
 	// example: https://epsi-rns.github.io /desktop/2018/05/04/openbox-config.html
 	// gets triggered every time the specific element goes visible/offscreen
+	// TODO: flashing/glitching animation?
 	const effects = ['slideup']; // for now, only a simple slideup gets used
 	const observer = new IntersectionObserver(ii => ii.forEach(i => {
 		const el = i.target;
-		const animate = el.dataset.animateonscroll;
+		let animate = el.dataset.animateonscroll;
 		if (effects.includes(animate)){
-			const effect = 'animate__' + animate;
-			if (i.isIntersecting){
-				el.classList.add(effect);
-			} else {
-				el.classList.remove(effect);
-			}
+			animate = 'animate__' + animate;
+			if (i.isIntersecting)
+				el.classList.add(animate);
+			else
+				el.classList.remove(animate);
 		}
 	}));
 	document.body.querySelectorAll("[data-animateonscroll]").forEach(iii => observer.observe(iii));
